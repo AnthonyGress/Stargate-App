2c8980ee9551ca37d51781b0e187ddcd7eb544f6
Anthony Gress <the.techx.guy@gmail.com>
2021-08-07 14:25:00 -0400

    housekeeping

--- testy2.js
+++
@@ -1,9 +0,0 @@
-const request = require("request");
-const options = {
-  method: "GET",
-  url: `// https://en.wikipedia.org/api/rest_v1/page/summary/${Body.name}`,
-};
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  console.log(body);
-});
