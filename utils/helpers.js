21cc97e9e5d0145fe446056ac98dcfa0011b66f4
Anthony Gress <the.techx.guy@gmail.com>
2021-08-08 14:07:50 -0400

    housekeeping

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,12 +3,4 @@ const format_date = (date) => {
   return date.toLocaleDateString();
 };
 
-const checkLocation = (comparator) => {
-  let currentUrl = window.location.href.toString().split("/").pop();
-  let result;
-  console.log(currentUrl);
-  currentUrl == comparator ? (result = true) : (result = false);
-  return result;
-};
-
-module.exports = { format_date, checkLocation };
+module.exports = { format_date };
