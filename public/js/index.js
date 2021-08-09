96deb17f1203213702142419cac76b892ff73c1d
Anthony Gress <the.techx.guy@gmail.com>
2021-08-09 15:47:02 -0400

    set zoom speed and rotation speed

--- public/js/index.js
+++ public/js/index.js
@@ -69,7 +69,8 @@ class BasicWorldDemo {
     this.orbitControls.autoRotate = true;
     this.orbitControls.autoRotateSpeed = 0.8;
     // this.orbitControls.dampingFactor = 0.05;
-    this.orbitControls.rotateSpeed = 0.15;
+    this.orbitControls.rotateSpeed = 0.25;
+    this.orbitControls.zoomSpeed = 0.35;
 
     this.orbitControls.update();
 
