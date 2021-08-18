a6e115651f600209ec6735579f31490871caeeb7
Anthony Gress <the.techx.guy@gmail.com>
2021-08-18 12:53:12 -0400

    toggle invisible modeldropdown on hamburger active

--- public/js/activeClass.js
+++ public/js/activeClass.js
@@ -5,6 +5,8 @@ const login = document.querySelector(".login");
 const potd = document.querySelector(".potd");
 const dashboard = document.querySelector(".dashboard");
 const searchForm = document.querySelector(".search-form");
+const hamburgerToggle = document.querySelector(".hamburger");
+const modelToggle = document.querySelector(".modelDropdownWrapper");
 
 let currentUrl = window.location.href.toString().split("/").pop();
 
@@ -22,7 +24,7 @@ if (currentUrl == "") {
 } else if (currentUrl == "potd") {
   potd.classList.add("active");
 }
-
+// search db
 function search_body(event) {
   event.preventDefault();
   let input = document.getElementById("body-term").value.trim().toLowerCase();
@@ -33,5 +35,27 @@ function search_body(event) {
   };
   getPlanet(url);
 }
+// hide model dropdown on hamburger open
+
+const hideModelDropdown = () => {
+  // if (modelToggle.classList.contains("fade-in")){
+  //   modelToggle.classList.remove("fade-in");
+  // }
+  // else {
+  //   modelToggle.classList.add("fade-in");  }
+  // bs invisible
+  modelToggle.classList.toggle("invisible");
+  // fade anim
+  // if (hamburgerToggle.classList.contains("collapsed")){
+  //   modelToggle.classList.remove("fade-out");
+  //   modelToggle.classList.add("fade-in");
+  // }
+  // else {
+  //   modelToggle.classList.remove("fade-in")
+  //   modelToggle.classList.add("fade-out")
+  // }
+}
+
 
 searchForm.addEventListener("submit", search_body);
+hamburgerToggle.addEventListener("click", hideModelDropdown);
